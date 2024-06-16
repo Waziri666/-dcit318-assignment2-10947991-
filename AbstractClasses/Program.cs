19c81a36924e86c027c0bb7e46fbaee493ef2ba9
Taufic Abdallah Waziri <tawaziri@st.ug.edu.gh>
2024-06-16 00:20:44 +0000

    Implemented Abstract Classes and Methods

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Circle and Rectangle
+            Circle circle = new Circle(5);
+            Console.WriteLine($"Area of circle: {circle.GetArea()}");
+
+            Rectangle rectangle = new Rectangle(4, 6);
+            Console.WriteLine($"Area of rectangle: {rectangle.GetArea()}");
+        }
+    }
+}
